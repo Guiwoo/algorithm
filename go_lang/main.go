388b689e34ea73de867ec78700577bb614b1e5a2
Guiwoo <park.guiwoo@hotmail.com>
2022-02-22 09:54:15 +0900

    algorithm python and go bj, hackerranks

--- go_lang/main.go
+++ go_lang/main.go
@@ -2,14 +2,37 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	num := 5
-	for i := 1; i <= num; i++ {
-		str1 := strings.Repeat(" ", num-i)
-		str2 := strings.Repeat("*", (i*2)-1)
-		fmt.Println(str1 + str2 + str1)
+	var (
+		first  int
+		second int
+		third  int
+	)
+
+	fmt.Scanf("%d %d %d", &first, &second, &third)
+
+	if first == second && second == third {
+		fmt.Println(10000 + (first)*1000)
+	} else if first == second || second == third || third == first {
+		value := 0
+		if first == second {
+			value = first
+		} else if second == third {
+			value = second
+		} else {
+			value = third
+		}
+		fmt.Println(1000 + (value)*100)
+	} else {
+		value := first
+		if value < second {
+			value = second
+		}
+		if value < third {
+			value = third
+		}
+		fmt.Println(value * 100)
 	}
 }
--- python/test.py
+++ python/test.py
@@ -1,24 +1,15 @@
-from tkinter import *
-
-def drawCircle(x, y, r):
-    global count
-    count += 1
-    canvas.create_oval(x - r, y - r, x + r, y + r)
-    canvas.create_text(x, y - r, text=str(count), font=("", 30))
-
-    if r >= radius / 2:
-        drawCircle(x - r // 2, y, r // 2)
-        drawCircle(x + r // 2, y, r // 2)
-
-
-count = 0
-wSize = 1000
-radius = 200
-
-window = Tk()
-canvas = Canvas(window, height=wSize, width=wSize, bg="white")
-
-drawCircle(wSize // 2, wSize // 2, radius)
-
-canvas.pack()
-window.mainloop()
+def arrayManipulation(n, queries):
+    # delimit each interval [a, b] by saving
+    # in deltas the difference that such
+    # interval has in respect of adjacent areas
+    deltas = [0] * (n + 1)
+    for a, b, k in queries:
+        deltas[a - 1] += k
+        deltas[b] -= k
+    # curr is like a walker going through mountains,
+    # and peek is the maximum height he/she reached
+    curr, peek = 0, 0
+    for k in deltas:
+        curr += k
+        peek = max(peek, curr)
+    return peek
