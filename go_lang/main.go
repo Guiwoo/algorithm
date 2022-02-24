9995ca9a00cdaac0b142958f13134ed939a606d1
Guiwoo <park.guiwoo@hotmail.com>
2022-02-25 00:29:05 +0900

    d-3

--- go_lang/main.go
+++ go_lang/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"strconv"
 )
 
 // Bufio Way
@@ -53,6 +55,7 @@ func back10952() {
 
 }
 
+//while 처럼 쓰기
 func back10951() {
 	var a, b int
 	reader := bufio.NewReader(os.Stdin)
@@ -68,21 +71,144 @@ func back10951() {
 	}
 }
 
-func main() {
-	var a, count int
+//최대값 찾기 ~
+func bj2562() {
+	var a int
 	reader := bufio.NewReader(os.Stdin)
 	writer := bufio.NewWriter(os.Stdout)
 	defer writer.Flush()
 	fmt.Fscanln(reader, &a)
-	k := a
-	for true {
-		first := a / 10
-		second := a % 10
-		a = (second)*10 + (first+second)%10
-		count++
-		if a == k {
-			break
+
+	var arr = make([]int, a)
+	for i := range arr {
+		fmt.Fscanf(reader, "%d", &arr[i])
+	}
+	min := arr[0]
+	max := arr[0]
+	for _, value := range arr {
+		if value < min {
+			min = value
+		}
+		if value > max {
+			max = value
 		}
 	}
-	fmt.Println(count)
+	fmt.Println(min, max)
+}
+
+func bj2577() {
+	a := 1
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	var arr = make([]int, 10)
+	for i := 0; i < 3; i++ {
+		var b int
+		fmt.Fscanln(reader, &b)
+		a = a * b
+	}
+	s := strconv.Itoa(a)
+	for _, v := range s {
+		v := string(v)
+		i, _ := strconv.Atoi(v)
+		arr[i] += 1
+	}
+	for _, v := range arr {
+		fmt.Println(v)
+	}
+}
+
+//여러값주고 안에서 나머지 값 찾기
+func bj3052() {
+	a := 42
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	var arr = make(map[int]bool)
+	for i := 0; i < 10; i++ {
+		var b int
+		fmt.Fscanln(reader, &b)
+		arr[b%a] = true
+	}
+	fmt.Println(arr)
+	fmt.Println(len(arr))
+}
+
+func bj1546() {
+	var a int
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	fmt.Fscanln(reader, &a)
+	defer writer.Flush()
+
+	var arr = make([]float64, a)
+	var high float64
+	for i, _ := range arr {
+		fmt.Fscanf(reader, "%v", &arr[i])
+		if arr[i] > high {
+			high = arr[i]
+		}
+	}
+	var total float64
+	for _, v := range arr {
+		total += (v * 100) / high
+	}
+	fmt.Println(total / float64(a))
+}
+
+func bj8958() {
+	var a int
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	fmt.Fscanln(reader, &a)
+	defer writer.Flush()
+	for i := 0; i < a; i++ {
+		var count, total int
+		var s string
+		fmt.Fscanln(reader, &s)
+		for _, v := range s {
+			if string(v) == "O" {
+				count++
+				total += count
+			} else {
+				count = 0
+			}
+		}
+		fmt.Println(total)
+	}
+}
+
+//시발 스페이스 하나 띄어쓰기 안해가지고 개빡치네 ㅅㅂ
+func bj4344() {
+	var c int
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	fmt.Fscanln(reader, &c)
+	defer writer.Flush()
+
+	for i := 0; i < c; i++ {
+		var n int
+		fmt.Fscanf(reader, "%d ", &n)
+
+		var scores = make([]float64, n)
+		var sum, avg float64
+
+		for j := 0; j < n; j++ {
+			fmt.Fscanf(reader, "%f ", &scores[j])
+			sum += scores[j]
+		}
+		avg = sum / float64(n)
+
+		var proportion float64
+		for _, val := range scores {
+			if val > avg {
+				proportion += (1 / float64(n))
+			}
+		}
+		fmt.Fprintf(writer, "%.3f%s\n", math.Round(proportion*100000)/1000, "%")
+	}
+}
+
+func main() {
+	bj4344()
 }
--- python/test.py
+++ python/test.py
@@ -18,21 +18,92 @@ def arrayManipulation(n, queries):
     return peek
 
 
-x = int(input())
-count = 0
-newX = None
-while True:
-    if x < 0 and newX == None:
-        newX = x * 10 + x
+def bj10181():
+    x = int(input())
+    count = 0
+    newX = None
+    while True:
+        if x < 0 and newX == None:
+            newX = x * 10 + x
+            count += 1
+            continue
+        if newX == None:
+            newX = (x % 10) * 10 + (x // 10 + x % 10) % 10
+        else:
+            newX = (newX % 10) * 10 + (newX // 10 + newX % 10) % 10
+
         count += 1
-        continue
-    if newX == None:
-        newX = (x % 10) * 10 + (x // 10 + x % 10) % 10
-    else:
-        newX = (newX % 10) * 10 + (newX // 10 + newX % 10) % 10
+        if x == newX:
+            break
 
-    count += 1
-    if x == newX:
-        break
+    print(count)
 
-print(count)
+
+def bj10818():
+    n = int(input())
+    arr = list(map(int, input().split()))
+    print(f"{min(arr)} {max(arr)}")
+
+
+def bj2562():
+    highest = 0
+    count = 0
+    for i in range(1, 10):
+        n = int(input())
+        if n > highest:
+            highest = n
+            count = i
+    print(highest, count, sep="\n")
+
+
+def bj2575():
+    a = 1
+    arr = [0] * 10
+    for _ in range(3):
+        n = int(input())
+        a *= n
+
+    for x in str(a):
+        arr[int(x)] += 1
+
+    print(*arr, sep="\n")
+
+
+def bj3052():
+    a = 42
+    mSet = set()
+    for _ in range(10):
+        n = int(input())
+        mSet.add(n % a)
+
+    print(mSet)
+    print(len(mSet))
+
+
+def bj1546():
+    total = 0
+    n = int(input())
+    arr = list(map(int, input().split()))
+
+    high = max(arr)
+    for i in arr:
+        total += float(i * 100) / float(high)
+    print(total / n)
+
+
+def bj8958():
+    num = int(input())
+    for _ in range(num):
+        total = 0
+        count = 0
+        arr = [*input()]
+        for i in arr:
+            if i == "O":
+                count += 1
+                total += count
+            else:
+                count = 0
+        print(total)
+
+
+bj8958()
