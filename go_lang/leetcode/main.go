0fe2aec317c419f8b8e6ac1fb9fd97fde5bc52a5
Guiwoo <park.guiwoo@hotmail.com>
2022-03-07 00:20:42 +0900

    leetcode

--- go_lang/leetcode/main.go
+++ go_lang/leetcode/main.go
@@ -1,13 +1,41 @@
-package leetcode
+package main
+
+import "fmt"
+
+func searchNum(nums []int, target int) (int, bool) {
+	for i, v := range nums {
+		if v == target {
+			return i, true
+		}
+	}
+	return 0, false
+}
 
 func twoSum(nums []int, target int) []int {
-	// for i,v := range nums{
-	//     // v 를 뺴버려 target에서 그리고target 값이 있으면 ? 끝내 없으면 ? 컨티뉴 ?낫베ㅡㄷ ?
+	answer := []int{}
+	if len(nums) == 2 {
+		return []int{0, 1}
+	}
+	for i, v := range nums {
+		// v 를 뺴버려 target에서 그리고target 값이 있으면 ? 끝내 없으면 ? 컨티뉴 ?낫베ㅡㄷ ?
+		realTarget := target - v
+		new := []int{}
 
-	// }
-	return []int{1, 2}
+		new = append(new, nums[0:i]...)
+		new = append(new, nums[i+1:]...)
+
+		index, found := searchNum(new, realTarget)
+		if found {
+			answer = append(answer, i)
+			answer = append(answer, index+1)
+			break
+		}
+	}
+	return answer
 }
 
 func main() {
-	// twoSum()
+	var arr = []int{3, 3}
+	answer := twoSum(arr, 9)
+	fmt.Println(answer)
 }
