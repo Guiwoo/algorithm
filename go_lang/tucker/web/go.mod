ffa9909e9e991f1fab3e6385d8d905e8ffe0ee3b
Guiwoo <park.guiwoo@hotmail.com>
2022-04-12 21:28:30 +0900

    pat, render, negrori

--- go_lang/tucker/web/go.mod
+++ go_lang/tucker/web/go.mod
@@ -4,11 +4,16 @@ go 1.18
 
 require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/pat v1.0.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/smartystreets/goconvey v1.7.2 // indirect
 	github.com/stretchr/testify v1.7.1 // indirect
 	github.com/tuckersGo/goWeb v0.0.0-20200531032145-28d77cfd0a29 // indirect
+	github.com/unrolled/render v1.4.1 // indirect
+	golang.org/x/sys v0.0.0-20210525143221-35b2ab0089ea // indirect
 	golang.org/x/tools v0.0.0-20190328211700-ab21143f2384 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- go_lang/tucker/web/main.go
+++ go_lang/tucker/web/main.go
@@ -1,9 +1,58 @@
 package main
 
-const PORT = ":3000"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/pat"
+	"github.com/unrolled/render"
+)
+
+var rd render.Render
+
+type User struct {
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func getUserInfoHandler(rw http.ResponseWriter, r *http.Request) {
+	user := User{Name: "guiwoo", Email: "test@test.com"}
+
+	rd.JSON(rw, http.StatusOK, user)
+}
+
+func addUserHandler(rw http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		rw.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(rw, err)
+		rd.Text(rw, http.StatusBadRequest, err.Error())
+		return
+	}
+	user.CreatedAt = time.Now()
+	data, _ := json.Marshal(user)
+	fmt.Fprint(rw, string(data))
+}
+
+func helloHandler(rw http.ResponseWriter, r *http.Request) {
+	user := User{Name: "guiwoo", Email: "test@test.com"}
+	rd.HTML(rw, http.StatusOK, "hello", user)
+}
 
 func main() {
-	// fmt.Printf("Server Connected http://localhost%s\n", PORT)
-	// http.ListenAndServe(PORT, rest.NewHandler())
+	rd = *render.New(render.Options{
+		Extensions: []string{".html", ".tmpl"},
+		Layout:     "hello",
+	})
+	mux := pat.New()
 
+	mux.Get("/users", getUserInfoHandler)
+	mux.Post("/users", addUserHandler)
+	mux.Get("/hello", helloHandler)
+
+	http.ListenAndServe(":3000", mux)
 }
