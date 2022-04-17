2f7745f9a548565301f5093757e2992eaffae87b
Guiwoo <park.guiwoo@hotmail.com>
2022-04-17 22:43:53 +0900

    testing and refectoring

--- go_lang/tucker/web/todo/app/app.go
+++ go_lang/tucker/web/todo/app/app.go
@@ -1,45 +1,28 @@
 package app
 
 import (
-	"fmt"
 	"net/http"
 	"strconv"
-	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/guiwoo/tucker_web/todo/model"
 	"github.com/unrolled/render"
 )
 
 var rd *render.Render
-
-type Todo struct {
-	ID        int       `json:"id"`
-	Name      string    `json:"name"`
-	Completed bool      `json:"completed"`
-	CreatedAt time.Time `json:"created_at"`
-}
-
-var todoMap map[int]*Todo
 
 func indexHandler(rw http.ResponseWriter, r *http.Request) {
 	http.Redirect(rw, r, "/todo.html", http.StatusTemporaryRedirect)
 }
 
 func getTodosHandler(rw http.ResponseWriter, r *http.Request) {
-	list := []*Todo{}
-	for _, v := range todoMap {
-		fmt.Println(v)
-		list = append(list, v)
-	}
+	list := model.GetTodos()
 	rd.JSON(rw, http.StatusOK, list)
 }
 
 func postTodosHandler(rw http.ResponseWriter, r *http.Request) {
 	name := r.FormValue("name")
-	id := time.Now().Second() + 2
-	todo := &Todo{id, name, false, time.Now()}
-	todoMap[id] = todo
-
+	todo := model.AddTodo(name)
 	rd.JSON(rw, http.StatusCreated, todo)
 }
 
@@ -50,8 +33,8 @@ type Success struct {
 func deleteTodoHandler(rw http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)["id"]
 	id, _ := strconv.Atoi(vars)
-	if _, ok := todoMap[id]; ok {
-		delete(todoMap, id)
+	ok := model.RemoveTodo(id)
+	if ok {
 		rd.JSON(rw, http.StatusOK, Success{true})
 	} else {
 		rd.JSON(rw, http.StatusOK, Success{false})
@@ -62,18 +45,15 @@ func toggleHandler(rw http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)["id"]
 	id, _ := strconv.Atoi(vars)
 	complete := r.FormValue("complete") == "true"
-
-	if todo, ok := todoMap[id]; ok {
-		todo.Completed = complete
+	ok := model.CompleteTodo(id, complete)
+	if ok {
 		rd.JSON(rw, http.StatusOK, Success{true})
 	} else {
 		rd.JSON(rw, http.StatusOK, Success{false})
-		return
 	}
 }
 
 func MakeHandler() http.Handler {
-	todoMap = make(map[int]*Todo)
 
 	rd = render.New()
 	r := mux.NewRouter()
--- go_lang/tucker/web/todo/app/app_test.go
+++ go_lang/tucker/web/todo/app/app_test.go
@@ -2,11 +2,13 @@ package app
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"testing"
 
+	"github.com/guiwoo/tucker_web/todo/model"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -14,30 +16,74 @@ func TestTodos(t *testing.T) {
 	assert := assert.New(t)
 	ts := httptest.NewServer(MakeHandler())
 	defer ts.Close()
-
-	resp, err := http.PostForm(ts.URL+"/todos", url.Values{"name": {"test todo"}})
+	resp, err := http.PostForm(ts.URL+"/todos", url.Values{"name": {"Test todo"}})
 	assert.NoError(err)
 	assert.Equal(http.StatusCreated, resp.StatusCode)
-	var todo Todo
+
+	var todo model.Todo
+
 	err = json.NewDecoder(resp.Body).Decode(&todo)
 	assert.NoError(err)
-	assert.Equal(todo.Name, "test todo")
-	// id1 := todo.ID
+	assert.Equal(todo.Name, "Test todo")
+	id1 := todo.ID
 
-	resp, err = http.PostForm(ts.URL+"/todos", url.Values{"name": {"test todo2"}})
+	resp, err = http.PostForm(ts.URL+"/todos", url.Values{"name": {"Test todo2"}})
 	assert.NoError(err)
 	assert.Equal(http.StatusCreated, resp.StatusCode)
 	err = json.NewDecoder(resp.Body).Decode(&todo)
 	assert.NoError(err)
-	assert.Equal(todo.Name, "test todo2")
-	// id2 := todo.ID
+	assert.Equal(todo.Name, "Test todo2")
+	id2 := todo.ID
 
 	resp, err = http.Get(ts.URL + "/todos")
 	assert.NoError(err)
 	assert.Equal(http.StatusOK, resp.StatusCode)
-	todos := []*Todo{}
+	todos := []*model.Todo{}
 	err = json.NewDecoder(resp.Body).Decode(&todos)
-
 	assert.NoError(err)
 	assert.Equal(len(todos), 2)
+
+	for _, v := range todos {
+		if v.ID == id1 {
+			assert.Equal("Test todo", v.Name)
+		} else if v.ID == id2 {
+			assert.Equal("Test todo2", v.Name)
+		} else {
+			assert.Error(fmt.Errorf("Does not exist the id1 and id2"))
+		}
+	}
+
+	resp, err = http.Get(ts.URL + "/toggletodo/" + fmt.Sprint(id1) + "?complete=true")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+
+	resp, err = http.Get(ts.URL + "/todos")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	todos = []*model.Todo{}
+	err = json.NewDecoder(resp.Body).Decode(&todos)
+	assert.NoError(err)
+
+	for _, v := range todos {
+		if v.ID == id1 {
+			assert.True(v.Completed)
+		}
+	}
+
+	req, _ := http.NewRequest("DELETE", ts.URL+"/todos/"+fmt.Sprint(id1), nil)
+	resp, err = http.DefaultClient.Do(req)
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+
+	resp, err = http.Get(ts.URL + "/todos")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	todos = []*model.Todo{}
+	err = json.NewDecoder(resp.Body).Decode(&todos)
+	assert.NoError(err)
+	assert.Equal(len(todos), 1)
+
+	for _, v := range todos {
+		assert.Equal(v.ID, id2)
+	}
 }
