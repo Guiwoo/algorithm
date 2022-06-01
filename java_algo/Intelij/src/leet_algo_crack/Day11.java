200313610f8f318900a3c1b97c13aa2f1d19314b
Guiwoo <park.guiwoo@hotmail.com>
2022-06-02 01:10:44 +0900

    done

--- java_algo/Intelij/src/leet_algo_crack/Day11.java
+++ java_algo/Intelij/src/leet_algo_crack/Day11.java
@@ -7,8 +7,6 @@ import java.util.stream.IntStream;
 
 public class Day11 {
     public static void main(String[] args){
-        Lt784 lt = new Lt784();
-        System.out.println(lt.letterCasePermutation3("a1bz"));
     }
     class Lt77 {
         List<List<Integer>> result = new ArrayList<>();
@@ -31,79 +29,79 @@ public class Day11 {
             }
         }
     }
-}
-class Lt784{
-    class Solution {
-     public List<String> letterCasePermutation(String S) {
-         if (S == null) {
-             return new LinkedList<>();
-         }
-         Queue<String> queue = new LinkedList<>();
-         queue.offer(S);
-
-         for (int i = 0; i < S.length(); i++) {
-             if (Character.isDigit(S.charAt(i))) continue;
-             int size = queue.size();
-             for (int j = 0; j < size; j++) {
-                 String cur = queue.poll();
-                 char[] chs = cur.toCharArray();
-
-                 chs[i] = Character.toUpperCase(chs[i]);
-                 queue.offer(String.valueOf(chs));
-
-                 chs[i] = Character.toLowerCase(chs[i]);
-                 queue.offer(String.valueOf(chs));
-             }
-         }
-
-                 return new LinkedList<>(queue);
-     }
-        public List<String> letterCasePermutation2(String S) {
-            if (S == null) {
-                return new LinkedList<>();
-            }
-
-            List<String> res = new LinkedList<>();
-            helper(S.toCharArray(), res, 0);
-            return res;
-        }
-
-        public void helper(char[] chs, List<String> res, int pos) {
-            if (pos == chs.length) {
-                res.add(new String(chs));
-                return;
-            }
-            if (chs[pos] >= '0' && chs[pos] <= '9') {
-                helper(chs, res, pos + 1);
-                return;
-            }
-
-            chs[pos] = Character.toLowerCase(chs[pos]);
-            helper(chs, res, pos + 1);
-
-            chs[pos] = Character.toUpperCase(chs[pos]);
-            helper(chs, res, pos + 1);
-        }
-        public List<String> letterCasePermutation1(String s) {
-            List<String> list = new ArrayList<>();
-            String word = s.toLowerCase();
-            list.add(word);
-            for(int i = 0; i <= word.length() - 1; i++) {
-                List<String> temp = new ArrayList<>();
-                for(String item: list) {
-                    int val = item.charAt(i) - '0';
-                    if(0 <= val && val <= 9) continue;
-                    temp.add(capitalize(item, i));
+    class Lt784{
+        class Solution {
+            public List<String> letterCasePermutation(String S) {
+                if (S == null) {
+                    return new LinkedList<>();
                 }
-                list.addAll(temp);
-            }
-            return list;
-        }
+                Queue<String> queue = new LinkedList<>();
+                queue.offer(S);
 
-        public String capitalize(String word, int i) {
-            char[] arr = word.toCharArray();
-            arr[i] = (char) ((arr[i] - 'a') + 'A');
-            return new String(arr);
+                for (int i = 0; i < S.length(); i++) {
+                    if (Character.isDigit(S.charAt(i))) continue;
+                    int size = queue.size();
+                    for (int j = 0; j < size; j++) {
+                        String cur = queue.poll();
+                        char[] chs = cur.toCharArray();
+
+                        chs[i] = Character.toUpperCase(chs[i]);
+                        queue.offer(String.valueOf(chs));
+
+                        chs[i] = Character.toLowerCase(chs[i]);
+                        queue.offer(String.valueOf(chs));
+                    }
+                }
+
+                return new LinkedList<>(queue);
+            }
+            public List<String> letterCasePermutation2(String S) {
+                if (S == null) {
+                    return new LinkedList<>();
+                }
+
+                List<String> res = new LinkedList<>();
+                helper(S.toCharArray(), res, 0);
+                return res;
+            }
+
+            public void helper(char[] chs, List<String> res, int pos) {
+                if (pos == chs.length) {
+                    res.add(new String(chs));
+                    return;
+                }
+                if (chs[pos] >= '0' && chs[pos] <= '9') {
+                    helper(chs, res, pos + 1);
+                    return;
+                }
+
+                chs[pos] = Character.toLowerCase(chs[pos]);
+                helper(chs, res, pos + 1);
+
+                chs[pos] = Character.toUpperCase(chs[pos]);
+                helper(chs, res, pos + 1);
+            }
+            public List<String> letterCasePermutation1(String s) {
+                List<String> list = new ArrayList<>();
+                String word = s.toLowerCase();
+                list.add(word);
+                for(int i = 0; i <= word.length() - 1; i++) {
+                    List<String> temp = new ArrayList<>();
+                    for(String item: list) {
+                        int val = item.charAt(i) - '0';
+                        if(0 <= val && val <= 9) continue;
+                        temp.add(capitalize(item, i));
+                    }
+                    list.addAll(temp);
+                }
+                return list;
+            }
+
+            public String capitalize(String word, int i) {
+                char[] arr = word.toCharArray();
+                arr[i] = (char) ((arr[i] - 'a') + 'A');
+                return new String(arr);
+            }
         }
     }
 }
