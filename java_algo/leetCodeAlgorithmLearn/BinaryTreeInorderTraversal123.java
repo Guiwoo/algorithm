5129673821a213778bd28e959b678b49b43a3407
Guiwoo <park.guiwoo@hotmail.com>
2022-06-19 20:19:09 +0900

    boj and leetcode

--- java_algo/leetCodeAlgorithmLearn/BinaryTreeInorderTraversal123.java
+++ java_algo/leetCodeAlgorithmLearn/BinaryTreeInorderTraversal123.java
@@ -1,11 +1,8 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
-public class BinaryTreeInorderTraversal {
-    public static void main(String[] args) {
-        // TreeNode tn = new TreeNode(1, null, new TreeNode(2, new TreeNode(3, null,
-        // null), null));
-        // System.out.println(inorderTraversal(tn));
-    }
+public class BinaryTreeInorderTraversal123 {
 
     static public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
--- java_algo/leetCodeAlgorithmLearn/Testing.java
+++ java_algo/leetCodeAlgorithmLearn/Testing.java
@@ -1,36 +1,28 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.stream.IntStream;
 
 public class Testing {
     public static void main(String[] args) {
-        IntersectionOfTwoArrays i = new IntersectionOfTwoArrays();
-        int[] nums1 = { 4, 9, 5 };
-        int[] nums2 = { 9, 4, 9, 8, 4 };
-        i.intersect(nums1, nums2);
+        MaxPorfit m = new MaxPorfit();
+        int rs = m.maxProfit(new int[] { 7, 1, 5, 3, 6, 4 });
+        System.out.println(rs);
     }
 }
 
-class IntersectionOfTwoArrays {
-    public int[] intersect(int[] nums1, int[] nums2) {
-        List<Integer> result = new ArrayList();
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int r : nums1) {
-            map.put(r, map.getOrDefault(r, 0) + 1);
-        }
+class MaxPorfit {
+    public int maxProfit(int[] prices) {
+        int lowestPrice = Integer.MAX_VALUE;
+        int profit = 0;
+        int current = 0;
 
-        for (int i = 0; i < nums2.length; i++) {
-            int target = nums2[i];
-            if (map.containsKey(target) && map.get(target) > 0) {
-                map.put(target, map.get(target) - 1);
-                result.add(target);
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < lowestPrice) {
+                lowestPrice = prices[i];
+            }
+            current = prices[i] - lowestPrice;
+            if (profit < current) {
+                profit = current;
             }
         }
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        return profit;
     }
 }
