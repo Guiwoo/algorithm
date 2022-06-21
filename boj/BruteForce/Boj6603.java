fe4314e555310a7e6725df2878a43911943491d4
Guiwoo <park.guiwoo@hotmail.com>
2022-06-21 10:40:06 +0900

    synchronize

--- boj/BruteForce/Boj6603.java
+++ boj/BruteForce/Boj6603.java
@@ -1,9 +1,7 @@
 package BruteForce;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
 
 public class Boj6603 {
     public static void main(String[] args) throws IOException {
--- boj/Test.java
+++ boj/Test.java
@@ -1,7 +1,6 @@
-import java.util.*;
+import java.io.IOException;
 
 public class Test {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
     }
 }
