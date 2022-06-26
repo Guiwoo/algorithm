487af2bb543a24daba69b02c87d121eebf805b86
Guiwoo <park.guiwoo@hotmail.com>
2022-06-26 22:54:36 +0900

    boj bruteforce

--- boj/BruteForce/Boj1251.java
+++ boj/BruteForce/Boj1251.java
@@ -1,7 +1,10 @@
+package BruteForce;
+
 import java.io.*;
 import java.util.*;
 
-public class Test {
+public class Boj1251 {
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
