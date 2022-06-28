8d64f987a9ddb902156d8f244228512a831f0825
Guiwoo <park.guiwoo@hotmail.com>
2022-06-28 19:20:48 +0900

    Ps0629 example.md file update

--- java_algo/leetCodeAlgorithmLearn/Testing.java
+++ java_algo/leetCodeAlgorithmLearn/Testing.java
@@ -3,22 +3,37 @@ import java.util.*;
 public class Testing {
   public static void main(String[] args) {
     Solution s = new Solution();
+    ListNode n = new ListNode(3);
+    ListNode n1 = new ListNode(2);
+    ListNode n2 = new ListNode(0);
+    ListNode n3 = new ListNode(-4);
+  }
+}
 
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode(int x) {
+    val = x;
+    next = null;
   }
 }
 
 class Solution {
-  public boolean isAnagram(String s, String t) {
-    if (s.length() != t.length())
+  public boolean hasCycle(ListNode head) {
+    Map<ListNode, Integer> map = new HashMap<>();
+    ListNode cur = head;
+    int idx = 0;
+    if (cur == null)
       return false;
-    char[] s1 = s.toCharArray();
-    char[] t1 = t.toCharArray();
-    Arrays.sort(s1);
-    Arrays.sort(t1);
-    for (int i = 0; i < s1.length; i++) {
-      if (s1[i] != t1[i])
-        return false;
+    while (!map.containsKey(cur) && cur != null) {
+      map.put(cur, idx);
+      idx++;
+      cur = cur.next;
     }
-    return true;
+    if (map.containsKey(cur))
+      return true;
+    return false;
   }
 }
