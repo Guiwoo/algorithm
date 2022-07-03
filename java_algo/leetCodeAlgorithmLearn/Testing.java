d9bcf7338b5cc6bbbcd83a811a937c89d3fa2b2f
Guiwoo <park.guiwoo@hotmail.com>
2022-07-03 23:26:21 +0900

    find first and last position

--- java_algo/leetCodeAlgorithmLearn/Testing.java
+++ java_algo/leetCodeAlgorithmLearn/Testing.java
@@ -1,29 +1,60 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Testing {
   public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String[] inputs = br.readLine().split(" ");
-    int[] arr = new int[Integer.parseInt(inputs[0])];
-    int day = Integer.parseInt(inputs[1]);
+    Solution s = new Solution();
+    int[] arr = { 5, 7, 7, 8, 8, 10 };
+    s.searchRange(arr, 8);
+  }
+}
 
-    String[] input2 = br.readLine().split(" ");
-    for (int i = 0; i < input2.length; i++) {
-      arr[i] = Integer.parseInt(input2[i]);
+class Solution {
+  public int[] searchRange(int[] arr, int target) {
+    int left = -1;
+    int right = -1;
+
+    int nLeft = 0;
+    int nRight = arr.length - 1;
+    int idx = -1;
+    while (nLeft <= nRight) {
+      int mid = nLeft + (nRight - nLeft) / 2;
+      if (arr[mid] < target) {
+        nLeft = mid + 1;
+      } else if (arr[mid] == target) {
+        idx = mid;
+        break;
+      } else {
+        nRight = mid - 1;
+      }
     }
 
-    int curSum = 0;
-    int max = 0;
-    for (int i = 0; i < day; i++) {
-      curSum += arr[i];
-      max += arr[i];
-    }
+    if (idx == -1)
+      return new int[] { left, right };
 
-    for (int i = day; i < arr.length - day + 1; i++) {
-      curSum += (arr[i] - arr[i - day]);
-      max = Math.max(curSum, max);
+    left = idx;
+    right = idx;
+
+    boolean leftMove = true;
+    boolean rightMove = true;
+
+    while (leftMove || rightMove) {
+      while (leftMove) {
+        if (left - 1 >= 0 && arr[left - 1] == target) {
+          left--;
+        } else {
+          leftMove = false;
+        }
+      }
+      while (rightMove) {
+        if (right + 1 < arr.length && arr[left - 1] == target) {
+          right++;
+        } else {
+          rightMove = false;
+        }
+      }
     }
-    System.out.println(max);
+    System.out.println(left + " " + right);
+    return new int[] { left, right };
   }
 }
