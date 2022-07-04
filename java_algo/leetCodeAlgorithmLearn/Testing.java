249364eeae5f164a1911a6859854d292593b1b2e
Guiwoo <park.guiwoo@hotmail.com>
2022-07-04 20:30:10 +0900

    sort 101

--- java_algo/leetCodeAlgorithmLearn/Testing.java
+++ java_algo/leetCodeAlgorithmLearn/Testing.java
@@ -4,57 +4,31 @@ import java.util.Arrays;
 public class Testing {
   public static void main(String[] args) throws Exception {
     Solution s = new Solution();
-    int[] arr = { 5, 7, 7, 8, 8, 10 };
-    s.searchRange(arr, 8);
+    int[] arr = { 4, 5, 6, 7, 0, 1, 2 };
+    s.search(arr, 0);
   }
 }
 
 class Solution {
-  public int[] searchRange(int[] arr, int target) {
-    int left = -1;
-    int right = -1;
-
-    int nLeft = 0;
-    int nRight = arr.length - 1;
+  public int search(int[] nums, int target) {
+    // target exist checking
+    int left = 0;
+    int right = nums.length - 1;
     int idx = -1;
-    while (nLeft <= nRight) {
-      int mid = nLeft + (nRight - nLeft) / 2;
-      if (arr[mid] < target) {
-        nLeft = mid + 1;
-      } else if (arr[mid] == target) {
-        idx = mid;
+    while (left <= right) {
+      if (nums[left] == target) {
+        idx = left;
         break;
       } else {
-        nRight = mid - 1;
+        left++;
+      }
+      if (nums[right] == target) {
+        idx = right;
+        break;
+      } else {
+        right--;
       }
     }
-
-    if (idx == -1)
-      return new int[] { left, right };
-
-    left = idx;
-    right = idx;
-
-    boolean leftMove = true;
-    boolean rightMove = true;
-
-    while (leftMove || rightMove) {
-      while (leftMove) {
-        if (left - 1 >= 0 && arr[left - 1] == target) {
-          left--;
-        } else {
-          leftMove = false;
-        }
-      }
-      while (rightMove) {
-        if (right + 1 < arr.length && arr[left - 1] == target) {
-          right++;
-        } else {
-          rightMove = false;
-        }
-      }
-    }
-    System.out.println(left + " " + right);
-    return new int[] { left, right };
+    return idx;
   }
 }
--- java_algo/leetCodeAlgorithmLearn/algoCrackMid/FindFirstAndLastPosition.java
+++ java_algo/leetCodeAlgorithmLearn/algoCrackMid/FindFirstAndLastPosition.java
@@ -1,53 +1,25 @@
 package algoCrackMid;
 
 public class FindFirstAndLastPosition {
-    class Solution {
-        public int[] searchRange(int[] arr, int target) {
-            int left = -1;
-            int right = -1;
-
-            int nLeft = 0;
-            int nRight = arr.length - 1;
-            int idx = -1;
-            while (nLeft <= nRight) {
-                int mid = nLeft + (nRight - nLeft) / 2;
-                if (arr[mid] < target) {
-                    nLeft = mid + 1;
-                } else if (arr[mid] == target) {
-                    idx = mid;
-                    break;
-                } else {
-                    nRight = mid - 1;
-                }
-            }
-
-            if (idx == -1)
-                return new int[] { left, right };
-
-            left = idx;
-            right = idx;
-
-            boolean leftMove = true;
-            boolean rightMove = true;
-
-            while (leftMove || rightMove) {
-                while (leftMove) {
-                    if (left - 1 >= 0 && arr[left - 1] == target) {
-                        left--;
-                    } else {
-                        leftMove = false;
-                    }
-                }
-                while (rightMove) {
-                    if (right + 1 < arr.length && arr[right + 1] == target) {
-                        right++;
-                    } else {
-                        rightMove = false;
-                    }
-                }
-            }
-            System.out.println(left + " " + right);
-            return new int[] { left, right };
+    public int[] searchRange(int[] A, int target) {
+        int start = firstGreaterEqual(A, target);
+        if (start == A.length || A[start] != target) {
+            return new int[] { -1, -1 };
         }
+        return new int[] { start, firstGreaterEqual(A, target + 1) - 1 };
+    }
+
+    int firstGreaterEqual(int[] A, int target) {
+        int low = 0, high = A.length;
+        while (low < high) {
+            int mid = low + ((high - low) >> 1);
+            if (A[mid] < target) {
+                low = mid + 1;
+            } else {
+
+                high = mid;
+            }
+        }
+        return low;
     }
 }
