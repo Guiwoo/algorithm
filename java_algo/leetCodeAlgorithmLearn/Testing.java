f021a9bfdf1c26236b3e17d60a339b85da36d873
Guiwoo <park.guiwoo@hotmail.com>
2022-07-06 01:38:24 +0900

    조지고올게

--- java_algo/leetCodeAlgorithmLearn/Testing.java
+++ java_algo/leetCodeAlgorithmLearn/Testing.java
@@ -2,59 +2,9 @@ import java.io.*;
 
 public class Testing {
   public static void main(String[] args) throws Exception {
-    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-    int N = Integer.parseInt(bf.readLine());
-    int[][] arr = new int[N][N];
-    for (int i = 0; i < N; i++) {
-      String[] inputs = bf.readLine().split(" ");
-      for (int j = 0; j < inputs.length; j++) {
-        arr[i][j] = Integer.parseInt(inputs[j]);
-      }
-    }
-    bf.close();
-    Boj b = new Boj();
-    b.main(arr);
-  }
-}
+    int[][] matrix = { { 1, 4, 7, 11, 15 }, { 2, 5, 8, 12, 19 }, { 3, 6, 9, 16, 22 }, { 10, 13, 14, 17, 24 },
+        { 18, 21, 23, 26, 30 } };
+    int target = 20;
 
-class Boj {
-  int white = 0;
-  int blue = 0;
-  int[][] arr = {};
-
-  public void main(int arr[][]) {
-    this.arr = arr;
-    helper(0, 0, arr.length);
-    System.out.println(blue);
-    System.out.println(white);
-  }
-
-  public void helper(int row, int col, int n) {
-    if (checker(row, col, n)) {
-      if (arr[row][col] == 0) {
-        white++;
-      } else {
-        blue++;
-      }
-      return;
-    }
-    int half = n / 2;
-
-    helper(row, col, half);
-    helper(row, col + half, half);
-    helper(row + half, col, half);
-    helper(row + half, col + half, half);
-  }
-
-  public boolean checker(int row, int col, int n) {
-    int target = arr[row][col];
-    for (int i = row; i < row + n; i++) {
-      for (int j = col; j < col + n; j++) {
-        if (arr[i][j] != target) {
-          return false;
-        }
-      }
-    }
-    return true;
   }
 }
