8a08779dbdf58dacff438b500f5877dde9f3a9f3
Guiwoo <park.guiwoo@hotmail.com>
2022-07-05 16:45:48 +0900

    fixed

--- java_algo/leetCodeAlgorithmLearn/Testing.java
+++ java_algo/leetCodeAlgorithmLearn/Testing.java
@@ -1,34 +1,60 @@
 import java.io.*;
-import java.util.Arrays;
 
 public class Testing {
   public static void main(String[] args) throws Exception {
-    Solution s = new Solution();
-    int[] arr = { 4, 5, 6, 7, 0, 1, 2 };
-    s.search(arr, 0);
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(bf.readLine());
+    int[][] arr = new int[N][N];
+    for (int i = 0; i < N; i++) {
+      String[] inputs = bf.readLine().split(" ");
+      for (int j = 0; j < inputs.length; j++) {
+        arr[i][j] = Integer.parseInt(inputs[j]);
+      }
+    }
+    bf.close();
+    Boj b = new Boj();
+    b.main(arr);
   }
 }
 
-class Solution {
-  public int search(int[] nums, int target) {
-    // target exist checking
-    int left = 0;
-    int right = nums.length - 1;
-    int idx = -1;
-    while (left <= right) {
-      if (nums[left] == target) {
-        idx = left;
-        break;
+class Boj {
+  int white = 0;
+  int blue = 0;
+  int[][] arr = {};
+
+  public void main(int arr[][]) {
+    this.arr = arr;
+    helper(0, 0, arr.length);
+    System.out.println(blue);
+    System.out.println(white);
+  }
+
+  public void helper(int row, int col, int n) {
+    if (checker(row, col, n)) {
+      if (arr[row][col] == 0) {
+        white++;
       } else {
-        left++;
+        blue++;
       }
-      if (nums[right] == target) {
-        idx = right;
-        break;
-      } else {
-        right--;
+      return;
+    }
+    int half = n / 2;
+
+    helper(row, col, half);
+    helper(row, col + half, half);
+    helper(row + half, col, half);
+    helper(row + half, col + half, half);
+  }
+
+  public boolean checker(int row, int col, int n) {
+    int target = arr[row][col];
+    for (int i = row; i < row + n; i++) {
+      for (int j = col; j < col + n; j++) {
+        if (arr[i][j] != target) {
+          return false;
+        }
       }
     }
-    return idx;
+    return true;
   }
 }
