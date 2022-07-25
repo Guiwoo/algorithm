8acf878361f4b1aac8ffd5c56ec125bd652fd161
Guiwoo <park.guiwoo@hotmail.com>
2022-07-25 10:26:07 +0900

    letetcode75 day1

--- java_algo/leetCodeAlgorithmLearn/Testing.java
+++ java_algo/leetCodeAlgorithmLearn/Testing.java
@@ -5,17 +5,18 @@ import java.util.*;
 public class Testing {
   public static void main(String[] args) throws Exception {
     Solution s = new Solution();
-    System.out.println(s.pivotIndex(new int[] { 2, 1, -1 }));
+    System.out.println(s.pivotIndex(new int[] { 1, 7, 3, 6, 5, 6 }));
   }
 }
 
 class Solution {
   public int pivotIndex(int[] nums) {
     int total = Arrays.stream(nums).sum();
-    int sum = 0;
-    for (int i = 0; i < nums.length; sum += nums[i++]) {
-      if (sum * 2 == total - nums[i])
+    int cur = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (cur == total - cur - nums[i])
         return i;
+      cur += nums[i];
     }
     return -1;
   }
