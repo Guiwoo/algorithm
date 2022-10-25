f39416c47e59f2dd2e80f0fa7f17a81a8abcfb5a
Guiwoo <park.guiwoo@hotmail.com>
2022-10-25 23:36:05 +0900

    boj1254

--- java_algo/Intelij/src/Testing.java
+++ java_algo/Intelij/src/Testing.java
@@ -1,9 +1,29 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Testing{
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String input = bf.readLine();
+        int ans = args.length;
+        for (int i = 0; i < input.length(); i++) {
+            if(isPal(input.substring(i))){
+                break;
+            }
+            ans++;
+        }
+        System.out.println(ans);
+    }
+    static boolean isPal(String s){
+        int start = 0;
+        int last = s.length()-1;
+        while(start<=last){
+            if(s.charAt(start) != s.charAt(last)) return false;
+            start++;
+            last--;
+        }
+        return true;
     }
 }
 
