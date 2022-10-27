f8f63cc8fca0c9ce9aa5573bfbd8c2bf9d83b694
Guiwoo <park.guiwoo@hotmail.com>
2022-10-27 19:25:54 +0900

    coding test week 23 done

--- java_algo/Intelij/src/Testing.java
+++ java_algo/Intelij/src/Testing.java
@@ -1,31 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Testing{
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String input = bf.readLine();
-        int ans = args.length;
-        for (int i = 0; i < input.length(); i++) {
-            if(isPal(input.substring(i))){
-                break;
-            }
-            ans++;
-        }
-        System.out.println(ans);
-    }
-    static boolean isPal(String s){
-        int start = 0;
-        int last = s.length()-1;
-        while(start<=last){
-            if(s.charAt(start) != s.charAt(last)) return false;
-            start++;
-            last--;
-        }
-        return true;
+
     }
 }
-
 
 
--- java_algo/Intelij/src/zerobase/codingTest_week_23/CodingTest_week_23.java
+++ java_algo/Intelij/src/zerobase/codingTest_week_23/CodingTest_week_23.java
@@ -1,121 +1,164 @@
 package zerobase.codingTest_week_23;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class CodingTest_week_23 {
-    class Solution2 {
-        public int[][] solution(int[][] s, int[][] t) {
-            TreeMap<Integer,Integer> map = new TreeMap<>();
-            List<Integer> answerList = new ArrayList<>();
-            Set<Integer> filter = new HashSet<>();
-
-            Deque<int[]> sQ = new ArrayDeque<>();
-            for(int[] ss: s) sQ.offer(ss);
-            Deque<int[]> tQ = new ArrayDeque<>();
-            for(int[] tt:t) tQ.offer(tt);
-
-            while(!sQ.isEmpty()&&!tQ.isEmpty()){
-                int[] sPoll = sQ.poll();
-                int[] tPoll = tQ.poll();
-
-                int minNum = Math.min(sPoll[1],tPoll[1]);
-                if(minNum == 0){
-                    if(sPoll[1] != minNum){
-                        sQ.offerFirst(sPoll);
-                    }
-                    if(tPoll[1] != minNum){
-                        tQ.offerFirst(tPoll);
-                    }
-                    continue;
-                }
-                int multi = sPoll[0]*tPoll[0];
-                map.put(multi, map.getOrDefault(multi,0)+minNum);
-                if(filter.add(multi)){
-                    answerList.add(multi);
-                }
-
-                if(sPoll[1] - minNum != 0){
-                    sPoll[1]-=minNum;
-                    sQ.offerFirst(sPoll);
-                }
-                if(tPoll[1]-minNum != 0){
-                    tPoll[1]-=minNum;
-                    tQ.offerFirst(tPoll);
+    class Solution {
+        public int solution(int L, int U, int D) {
+            if(U >= L){
+                return 1;
+            }
+            int cnt =1;
+            int step = U-D;
+            while(true){
+                cnt++;
+                if((step*cnt)+U >= L){
+                    if(cnt == 2) return  cnt;
+                    return cnt+1;
                 }
             }
-            int[][] answer = new int[map.size()][2];
-            int x = 0;
-            for(Integer i : answerList){
-                answer[x++] = new int[]{i,map.get(i)};
-            }
-            return answer;
         }
     }
-    class Solution3{
-        public int[][] solution(int[][] s, int[][] t) {
-            int[][] answer = new int[s.length][t[0].length];
 
-            for(int i=0; i<answer.length; i++){
-                for(int j=0; j<answer[0].length; j++){
-                    int sum=0;
-                    for(int k=0; k<s[0].length; k++){
-                        sum += s[i][k]*t[k][j];
-                    }
-                    answer[i][j] = sum;
+    class Solution2 {
+        public int solution(String s, int N, int M) {
+            String[] input = s.split(" ");
+            int[] arr = new int[input.length];
+            for (int i = 0; i < input.length; i++) {
+                arr[i] = input[i].length();
+            }
+
+            for (int i = 1; i < arr.length; i++) {
+                arr[i]+=arr[i-1];
+            }
+
+            int cnt = 0;
+            int marker = 0;
+            for (int i = 0; i < arr.length; i++) {
+                int i1 = (marker == 0 ? arr[i] : arr[i] - arr[marker])+i;
+                if(i1 > M){
+                    cnt++;
+                    marker = i;
+                }
+            }
+            return N/cnt;
+        }
+    }
+
+    class Solution3 {
+        public int solution(int[] nums) {
+            int oneCnt = 0;
+            for (int num : nums) {
+                if(num == 1) oneCnt++;
+            }
+            int sliding = 0;
+            for (int i = 1; i <= nums.length; i++) {
+                if(oneCnt*2 < i) continue;
+                int i1 = sliding(i, nums) % 10007;
+                sliding+=i1;
+            }
+            System.out.println(sliding);
+            return sliding;
+        }
+        int sliding(int len,int[] nums){
+            int oneCnt = 0;
+            int zeroCnt = 0;
+            int left = 0;
+            int right = len;
+
+            for (int i = 0; i < right; i++) {
+                if(nums[i] == 0){
+                    zeroCnt++;
+                }else{
+                    oneCnt++;
+                }
+            }
+            int cnt = 0;
+            while(right < nums.length && left <nums.length){
+                if(oneCnt > zeroCnt){
+                    cnt++;
+                }
+
+                int num = nums[left++];
+                if(num == 0){
+                    zeroCnt--;
+                }else{
+                    oneCnt--;
+                }
+                int num2 = nums[right++];
+                if(num2 == 0){
+                    zeroCnt++;
+                }else{
+                    oneCnt++;
+                }
+            }
+            if(oneCnt > zeroCnt){
+                cnt++;
+            }
+            return cnt;
+        }
+    }
+
+    class Solution4 {
+        public int solution(int[][] image, int x, int y) {
+            int cnt = 0;
+            for (int[] ints : image) {
+                for (int anInt : ints) {
+                    if(anInt == 1) cnt ++;
                 }
             }
 
-            return answer;
+            int left =1001,right=0,top=1001,bottom=0;
+            int[] dirs = {0,1,0,-1,0};
+            int N = image.length;
+            int M = image[0].length;
+
+            if(cnt+M >= N) return N*M;
+            boolean[][] visit = new boolean[N][M];
+
+
+            Queue<int[]> q = new LinkedList<>();
+            q.offer(new int[]{x,y});
+            while(!q.isEmpty()){
+                int[] poll = q.poll();
+                int row = poll[0],col = poll[1];
+                if(visit[row][col]) continue;
+                if(row < top) top = row;
+                if(row > bottom) bottom = row;
+                if(col < left) left = col;
+                if(col >right) right = col;
+                visit[row][col] = true;
+
+                for(int i=1;i<dirs.length;i++){
+                    int nRow = row + dirs[i];
+                    int nCol = col + dirs[i-1];
+
+                    if(nRow<0 || nCol < 0 || nRow >= N || nCol >= M || visit[nRow][nCol] || image[nRow][nCol] == 0) continue;
+
+                    q.offer(new int[]{nRow,nCol});
+                }
+            }
+            return (right - left + 1) * (bottom - top + 1);
         }
+
     }
 
     class Solution5 {
-        char[][] work;
-        int total = 0;
-        public int solution(String[] words) {
-            //words 의 갯수 만큼 배열을 만들어서 다찾아버려
-            work = new char[words[0].length()][words[0].length()];
-            //for loop depth 를 어케정하지 ? words의 length 에 따라서
-            //콤비네이션으로 3개 뽑자.
-            String[] out = new String[words[0].length()];
-            boolean[] visit = new boolean[words.length];
-            combination(words,visit,out,0,0);
-            return total;
-        }
+        public int solution(int hungry, int price, int[] plastic, int[] chain) {
+            Queue<int[]> q = new PriorityQueue<>((x, y) ->y[0]*y[1] - x[0]*x[1]);
+            for (int i = 0; i < chain.length; i++) {
+                q.offer(new int[]{plastic[i],chain[i]});
+            }
+            int total = 0;
+            while(!q.isEmpty()){
+                int[] poll = q.poll();
+                if(poll[0] < hungry){
 
-        void combination(String[] words,boolean[] visit,String[] out,int start,int depth){
-            if(depth==out.length){
-                validate(out);
-                return;
-            }else{
-                for (int i = start; i < words.length; i++) {
-                    if(visit[i] != true){
-                        visit[i] = true;
-                        out[depth] = words[i];
-                        combination(words,visit,out,i+1,depth+1);
-                        visit[i]=false;
-                    }
                 }
             }
-        }
-        void validate(String[] s){
-            String target = s[0];
-            int cnt = 0;
-            for (int i = 0; i < target.length(); i++) {
-                for (int j = 1; j < s.length; j++) {
-                    if(s[j].startsWith(String.valueOf(target.charAt(j)))){
-                        cnt++;
-                        continue;
-                    }
-                    if(s[j].endsWith(String.valueOf(target.charAt(j)))){
-                        cnt++;
-                        continue;
-                    }
-                }
-            }
-            if(cnt == target.length()){
-                total++;
-            }
+            return 0;
         }
     }
 }
