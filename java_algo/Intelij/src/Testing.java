c4760cc29e050af47f53807dc1626185d3b558d2
Guiwoo <park.guiwoo@hotmail.com>
2022-11-30 23:56:41 +0900

    sync

--- java_algo/Intelij/src/Testing.java
+++ java_algo/Intelij/src/Testing.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Testing{
     public static void main(String[] args) throws IOException {
         Solution s = new Solution();
-        boolean valid = s.isValid("()");
+        boolean valid = s.isValid("((");
         System.out.println(valid);
 
     }
@@ -12,6 +12,25 @@ public class Testing{
 
 class Solution {
     public boolean isValid(String s) {
+        Map<Character,Character> map = new HashMap<>();
+        map.put(')','(');
+        map.put('}','{');
+        map.put(']','[');
 
+        Deque<Character> q = new ArrayDeque<>();
+        for(char c : s.toCharArray()){
+            if(c == ')' || c == '}' || c==']'){
+                if(q.isEmpty()) return false;
+                if(q.peekLast().equals(map.get(c))){
+                    q.pollLast();
+                }else{
+                    return false;
+                }
+            }else{
+                q.offer(c);
+            }
+        }
+        if(!q.isEmpty()) return false;
+        return true;
     }
 }
