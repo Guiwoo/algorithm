2014db603b4c64261d870606611ce2dbe6705ff8
Guiwoo <park.guiwoo@hotmail.com>
2022-12-13 00:16:31 +0900

    변경

--- java_algo/Intelij/src/leet/easy/MaximumProductOfSplittedBinaryTree.java
+++ java_algo/Intelij/src/leet/easy/MaximumProductOfSplittedBinaryTree.java
@@ -11,47 +11,28 @@ public class MaximumProductOfSplittedBinaryTree {
         int i = s.maxProduct(new TreeNode(1, new TreeNode(1), null));
         System.out.println(i);
     }
-}
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
-class Solution {
-    int total = 0;
-    int module = (int) Math.pow(10,9)+7;
-    public int maxProduct(TreeNode root) {
-        List<Integer> sum = new ArrayList<>();
-        int total = getTotalSum(root,sum);
-        int max = 0;
-        for (Integer target : sum) {
-            int t = (target * (total - target))%module;
-            max = Math.max(max,t);
+    static class Solution {
+        int total = 0;
+        int module = (int) Math.pow(10,9)+7;
+        public int maxProduct(leet.easy.TreeNode root) {
+            List<Integer> sum = new ArrayList<>();
+            int total = getTotalSum(root,sum);
+            int max = 0;
+            for (Integer target : sum) {
+                int t = (target * (total - target))%module;
+                max = Math.max(max,t);
+            }
+            return max;
         }
-        return max;
-    }
 
-    public int getTotalSum(TreeNode node, List<Integer> sum){
-        if(node == null) return 0;
-        int left = getTotalSum(node.left,sum);
-        int right = getTotalSum(node.right,sum);
-        int total = (left + right)%module;
-        sum.add(total);
-        return total;
+        public int getTotalSum(leet.easy.TreeNode node, List<Integer> sum){
+            if(node == null) return 0;
+            int left = getTotalSum(node.left,sum);
+            int right = getTotalSum(node.right,sum);
+            int total = (left + right)%module;
+            sum.add(total);
+            return total;
+        }
     }
 }
